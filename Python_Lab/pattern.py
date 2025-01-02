6db2a0ca322c8e68c47f41223f68c6b14e19fc69
Krishnendhu Jills <krishnendhujills@gmail.com>
2025-01-02 22:45:07 +0530

    Added a program

---
+++ Python_Lab/pattern.py
@@ -0,0 +1,9 @@
+for i in range(0,5):
+    for j in range(i+1):
+        print("*", end="")
+    print("\n")
+
+for i  in range(1,5):
+    for j in range(i,5):
+        print("*", end="")
+    print("\n")
